ac30ea04b1fc44822de28570aa310b3df29e80a6
Mohammed Usama <usamabhakrani@gmail.com>
2023-07-14 11:03:43 +0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = 'https://course-api.com/react-tours-project'
+
+function App() {
+  return <h2>Tours Project Setup</h2>
+}
+
+export default App
---
+++ src/Tour.jsx
@@ -0,0 +1,6 @@
+
+const Tour = () => {
+  return <h2>tour component</h2>;
+};
+
+export default Tour;
