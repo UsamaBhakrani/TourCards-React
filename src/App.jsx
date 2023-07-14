10862f75b39496a0dae18d03ac4ddf68a390e0fe
Mohammed Usama <usamabhakrani@gmail.com>
2023-07-14 11:41:02 +0500

    UseEffect,FetchData,Try/Catch

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
+import { useEffect, useState } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = "https://course-api.com/react-tours-project123";
 
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setLoading(false);
+      setTours(data);
+    } catch (err) {
+      setLoading(false);
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours />
+    </main>
+  );
 }
 
-export default App
+export default App;
--- src/Tours.jsx
+++ src/Tours.jsx
@@ -1,5 +1,13 @@
 const Tours = () => {
-  return <h2>tours component</h2>;
+  return (
+    <section>
+      <div className="title">
+        <h2>Our Tours</h2>
+        <div className="title-underline"></div>
+      </div>
+      <div className="tours"></div>
+    </section>
+  );
 };
 
 export default Tours;
